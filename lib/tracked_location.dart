99a0c53e09295aa4d5e75a907696c2bbdbd61e41
dejima <dejima@framelunch.jp>
2024-01-19 12:10:57 +0900

    update

--- lib/tracked_location.dart
+++ lib/tracked_location.dart
@@ -2,32 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:location_tracking_app_demo/traked_location_paint.dart';
 
-class STrackedLocation extends ConsumerWidget{
+final _paintType = StateProvider((ref) => PaintType.point);
+
+class STrackedLocation extends ConsumerWidget {
   final List beaconInfos;
   const STrackedLocation({required this.beaconInfos});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final mq = MediaQuery.of(context);
+    final canvasWidth = mq.size.width - 70;
+    final paintType = ref.watch(_paintType);
 
     return Scaffold(
       backgroundColor: Colors.blueGrey[900],
       body: SingleChildScrollView(
         child: Column(
           children: [
+            Switch(
+              value: paintType == PaintType.line,
+              onChanged: (val) {
+                if(paintType == PaintType.line){
+                  ref.read(_paintType.notifier).update((state) => PaintType.point);
+                  return;
+                }
+                ref.read(_paintType.notifier).update((state) => PaintType.line);
+              },
+            ),
             Container(
               margin: const EdgeInsets.symmetric(vertical: 50),
-              padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+              padding: const EdgeInsets.symmetric(vertical: 20),
               color: Colors.white,
               child: Center(
                 child: Container(
-                  width: 580,
-                  height: 435,
+                  width: canvasWidth,
+                  height: canvasWidth * 0.75,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     border: Border.all(width: 3, color: Colors.black87),
                   ),
                   child: CustomPaint(
-                    size: const Size(580, 435),
-                    painter: FootprintsPainter(beaconInfos: beaconInfos),
+                    painter: FootprintsPainter(beaconInfos: beaconInfos, paintType: paintType),
                   ),
                 ),
               ),
--- lib/traked_location_paint.dart
+++ lib/traked_location_paint.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
 
+enum PaintType {
+  point,
+  line,
+}
+
 class FootprintsPainter extends CustomPainter {
   final List beaconInfos;
-  FootprintsPainter({required this.beaconInfos});
+  final PaintType paintType;
+
+  FootprintsPainter({required this.beaconInfos, required this.paintType});
   @override
   void paint(Canvas canvas, Size size) async {
-
-    final paint = Paint()
-      ..color = Colors.blue.withOpacity(0.4)
-      // ..shader = LinearGradient(colors: colors)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 20
-      ..strokeCap = StrokeCap.round;
-
     final infos = beaconInfos;
     final rowNum = infos.length ~/ 9;
+    final magnification = size.width / 1160;
+
+    final paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 20 * magnification
+      ..strokeCap = StrokeCap.round;
+
     List<Map<String, String>> points = [];
+
     for (var i = 0; i < rowNum; i++) {
       final index = i * 9;
       final x = infos[index + 3];
@@ -25,12 +32,52 @@ class FootprintsPainter extends CustomPainter {
     }
 
     final pointsOffset = points.map((info) {
-      final x = int.parse(info['x']!) * 0.6 / 2;
-      final y = int.parse(info['y']!) * 0.6 / 2;
+      // cm -> px  * magnification
+      final x = int.parse(info['x']!) * 0.6 * magnification;
+      final y = int.parse(info['y']!) * 0.6 * magnification;
 
       return Offset(x, y);
     }).toList();
-    canvas.drawPoints(ui.PointMode.polygon, pointsOffset, paint);
+
+    if (paintType == PaintType.point) {
+      pointsOffset.asMap().forEach((i, value) {
+        final color = ColorTween(
+          begin: const Color(0xFF5CB3FB).withOpacity(0.4),
+          end: const Color(0xFFF6493D).withOpacity(0.4), // Change the end color as needed
+        ).lerp(i / (pointsOffset.length - 1));
+
+        paint.color = color!;
+        canvas.drawPoints(
+          ui.PointMode.points,
+          [pointsOffset[i]],
+          paint,
+        );
+      });
+      return;
+    }
+
+    for (int i = 0; i < pointsOffset.length; i++) {
+      final color = ColorTween(
+        begin: Colors.blue.withOpacity(0.4),
+        end: Colors.red.withOpacity(0.4), // Change the end color as needed
+      ).lerp(i / (pointsOffset.length - 1));
+
+      paint.color = color!;
+
+      if (i == pointsOffset.length - 1) {
+        canvas.drawLine(
+          pointsOffset[i],
+          pointsOffset[i],
+          paint,
+        );
+        continue;
+      }
+      canvas.drawLine(
+        pointsOffset[i],
+        pointsOffset[i + 1],
+        paint,
+      );
+    }
   }
 
   @override
